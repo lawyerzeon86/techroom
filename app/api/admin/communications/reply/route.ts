8649819c62f6e6c9d673902011911258e9b5f5b7
lawyerzeon86 <lawyer.zeon@gmail.com>
2026-09-12 15:28:52 +0300

    Fix communications reply imports

--- app/api/admin/communications/reply/route.ts
+++ app/api/admin/communications/reply/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
-import { isAdminSession, readJsonBody } from '../../../../../../lib/security';
-import { replyCommunication, type CommunicationType } from '../../../../../../lib/communications';
-import type { MarketplaceName } from '../../../../../../lib/marketplaces';
+import { isAdminSession, readJsonBody } from '../../../../../lib/security';
+import { replyCommunication, type CommunicationType } from '../../../../../lib/communications';
+import type { MarketplaceName } from '../../../../../lib/marketplaces';
 
 export const runtime='nodejs';
 export const dynamic='force-dynamic';
